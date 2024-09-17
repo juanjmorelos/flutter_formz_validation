68d74588898a95743a3ea818e443dd8f8dad70c3
Juan Morelos <jmorelos@magnetoglobal.com>
2024-09-17 16:06:27 -0500

    Se agrega scroll horizontal en terminal_body

--- lib/screens/json/components/terminal_body.dart
+++ lib/screens/json/components/terminal_body.dart
@@ -4,11 +4,24 @@ import 'package:first_flutter_app/screens/json/customTheme/atomCustomTheme.dart'
 import 'package:flutter/material.dart';
 import 'package:flutter_highlight/flutter_highlight.dart';
 
-class TerminalBody extends StatelessWidget {
+class TerminalBody extends StatefulWidget {
   const TerminalBody({super.key, required this.color, required this.content});
   final Color color;
   final String content;
+
+  @override
+  State<TerminalBody> createState() => _TerminalBodyState();
+}
+
+class _TerminalBodyState extends State<TerminalBody> {
   
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose(); // Descartar el controlador cuando el widget se destruya
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,22 +31,30 @@ class TerminalBody extends StatelessWidget {
       width: double.infinity,
       padding: const EdgeInsets.all(13),
       decoration: BoxDecoration(
-        color: color,
+        color: widget.color,
         borderRadius: BorderRadius.only(bottomLeft: Radius.circular(borderRadius), bottomRight: Radius.circular(borderRadius))
       ),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        
-        child: HighlightView(
-          decodeJson(content),
-          language: 'json',
-          theme: atomCustomDarkTheme(color: color),
-          padding: EdgeInsets.zero,  // Sin relleno adicional
-          textStyle: const TextStyle(
-            fontFamily: 'Jetbrains',
-            fontStyle: FontStyle.normal,
-            fontSize: 15,
-            color: Color.fromARGB(255, 202, 202, 202)
+      child: RawScrollbar(
+        controller: _scrollController,
+        thumbVisibility: true,
+        trackVisibility: true,
+        interactive: true,
+        thumbColor: const Color.fromRGBO(255, 255, 255, 0.6),
+        radius: const Radius.circular(10),
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          scrollDirection: Axis.horizontal,
+          child: HighlightView(
+            decodeJson(widget.content),
+            language: 'json',
+            theme: atomCustomDarkTheme(color: widget.color),
+            padding: EdgeInsets.zero,  // Sin relleno adicional
+            textStyle: const TextStyle(
+              fontFamily: 'Jetbrains',
+              fontStyle: FontStyle.normal,
+              fontSize: 15,
+              color: Color.fromARGB(255, 202, 202, 202)
+            ),
           ),
         ),
       ),
